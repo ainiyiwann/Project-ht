5330ca441ac4b4fccb69b3d5a22470685121bbdd
jack  zwj <95678550+ainiyiwann@users.noreply.github.com>
2022-05-27 12:29:49 +0800

    完成登录业务

---
+++ app/src/api/user.js
@@ -0,0 +1,27 @@
+import request from '@/utils/request'
+
+//登录的接口
+export function login(data) {
+  return request({
+    url: '/admin/acl/index/login',
+    method: 'post',
+    data
+  })
+}
+
+//获取用户信息的接口
+export function getInfo(token) {
+  return request({
+    url: '/admin/acl/index/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+//退出登录的接口
+export function logout() {
+  return request({
+    url: '/admin/acl/index/logout',
+    method: 'post'
+  })
+}
